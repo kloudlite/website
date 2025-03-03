b9dd86eb37bdf01c37a5b60e0f8f6d9d18b937af
Bikash <tulsiojhaamg@gmail.com>
2025-03-03 17:23:27 +0545

    moved firebase config to .env

--- next.config.mjs
+++ next.config.mjs
@@ -28,6 +28,13 @@ export default withNextra({
     CONSOLE_URL: process.env.CONSOLE_URL,
     CONTACT_URL: process.env.CONTACT_URL,
     NEXT_PUBLIC_RECAPTCHA_SITE_KEY: process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY,
+    FB_API_KEY: process.env.FB_API_KEY,
+    FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN,
+    FB_PROJECT_ID: process.env.FB_PROJECT_ID,
+    FB_STORAGE_BUCKET: process.env.FB_STORAGE_BUCKET,
+    FB_MESSAGING_SENDER_ID: process.env.FB_MESSAGING_SENDER_ID,
+    FB_APP_ID: process.env.FB_APP_ID,
+    FB_MEASUREMENT_ID: process.env.FB_MEASUREMENT_ID,
   },
   images: {
     unoptimized: true,
--- web/utils/useFirebase.tsx
+++ web/utils/useFirebase.tsx
@@ -8,14 +8,21 @@ import {
   useMemo,
 } from 'react';
 
-import config from '../../.firebaseConfig';
-
 const FirebaseContext = createContext<{ firebaseApp: FirebaseApp | null }>({
   firebaseApp: null,
 });
 
 const FirebaseProvider = ({ children }: { children?: ReactNode }) => {
-  const app = initializeApp(config);
+  const app = initializeApp({
+    apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
+    projectId: process.env.FB_PROJECT_ID,
+    storageBucket: process.env.FB_STORAGE_BUCKET,
+    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+    appId: process.env.FB_APP_ID,
+    measurementId: process.env.FB_MEASUREMENT_ID,
+  });
+
   useEffect(() => {
     getAnalytics(app);
   }, []);
